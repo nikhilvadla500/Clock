2eee43564f06ddeacbaf0df02c0d566ca1f554c4
Nikhil kumar <nikhilvadla500@gmail.com>
2024-10-23 22:11:45 +0530

    firstcommit

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+function Clock(){
+    let now = new Date();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
+
+    hours = hours < 10 ? "0" + hours: hours;
+    minutes = minutes < 10 ? "0" + minutes: minutes;
+    seconds = seconds < 10 ? "0" + seconds: seconds;
+
+    const Time = hours + ":" + minutes + ":" + seconds;
+    document.getElementById("clock").innerHTML = Time;
+}
+setInterval(Clock,1000)
+Clock;
+
 
 
